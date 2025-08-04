3944cc1495da5c89652cc70da5828b90d50741b3
Pedro <pedrogabrielbhz@gmail.com>
2025-08-04 20:34:28 -0300

    implement simple vga driver

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![no_std]
 #![no_main]
 
+mod vga_buffer;
 use core::panic::PanicInfo;
 
 static HELLO: &[u8] = b"Hello World!";
@@ -8,21 +9,13 @@ const COLORS: [u8; 7] = [0xc, 0xa, 0xe, 0x9, 0xd, 0xb, 0xf];
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        let color_byte = COLORS[i % COLORS.len()];
-
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = color_byte;
-        }
-    }
-
+    println!("Hello World{}", "!");
+    panic!("i'm so panicked, right now...");
     loop {}
 }
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
