de44b3db5e41ab231d8d2059468a5882fd6e4859
Pedro <pedrogabrielbhz@gmail.com>
2025-08-05 00:15:23 -0300

    heliosphan

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,20 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
-mod vga_buffer;
 use core::panic::PanicInfo;
-
-static HELLO: &[u8] = b"Hello World!";
-const COLORS: [u8; 7] = [0xc, 0xa, 0xe, 0x9, 0xd, 0xb, 0xf];
+mod vga_buffer;
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
-    panic!("i'm so panicked, right now...");
+
+    #[cfg(test)]
+    test_main();
+
+    //    panic!("i'm so panicked, right now...");
     loop {}
 }
 
@@ -18,4 +22,37 @@ pub extern "C" fn _start() -> ! {
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
+}
+
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+
+    exit_qemu(QemuExitCode::Success);
+}
+
+#[test_case]
+fn trivial_assertion() {
+    print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    println!("[ok]");
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
 }
